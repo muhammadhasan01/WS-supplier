fdc0a1c26ae8b4795318e3bf07c0089682237155
fabian_zhafransyah <13518022@std.stei.itb.ac.id>
2020-11-26 20:33:58 +0700

    List database finished:

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express')
-const app = express()
+const { connection } = require('./db.js')
 const port = 3000
 
+const app = express()
 app.get('/', (req, res) => {
-  res.send('')
+  res.send('ok')
+})
+
+app.get('/list', (req, res) => {
+  console.log('oke')
+  try {
+    const query = req.query.harga ? "SELECT id, nama, kadaluarsa FROM bahan" : "SELECT * FROM bahan"
+    const query_result = connection.query(query, function(err, rows, fields) {
+      res.send(rows)
+      console.log(rows)
+    })
+  } catch (e) {
+    console.log(e)
+    res.send("error")
+  }
 })
 
 app.listen(port, () => {
