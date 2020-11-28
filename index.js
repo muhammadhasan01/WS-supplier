f5f1bb6ece2f0f6d5c61000738f4fa12deeb0181
mzunanalfikri <zunanalfikri@gmail.com>
2020-11-28 18:23:23 +0700

    ganti port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 var bodyParser = require('body-parser');
 const { connection } = require('./db.js')
-const port = 3000
+const port = 8082
 
 const app = express()
 app.use(bodyParser.json());
